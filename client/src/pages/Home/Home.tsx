ec7787a185152c26aca9c28b00821e25f10cd847
lawang <yorktown1781@163.com>
2024-08-08 23:49:33 +0200

    feat: call google api in server

---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+const Home: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Discover New  Restaurants!</h1>
+      <h2>Top Choices</h2>
+    </div>
+  );
+};
+
+export default Home;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,50 @@
-import Fastify from 'fastify';
-import cors from '@fastify/cors';
+const Fastify = require('fastify');
+const cors = require('@fastify/cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const server = Fastify();
+const url = 'https://places.googleapis.com/v1/places:searchText';
+const requestBody = {
+  textQuery: 'Chinese Restaurant in Stockholm, Sweden'
+};
 
 server.register(cors, {
   origin: '*'
 });
 
 server.get('/api', async (request, reply) => {
-    return { message: 'Welcome to FoodGuide API!' };
-  });
+  return { message: 'Welcome to FoodGuide API!' };
+});
+
+server.get('/api/maps', async (request, reply) => {
+  try {
+    const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+
+    if (!apiKey) {
+      return reply.status(500).send({ error: 'API key not configured' });
+    }
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': apiKey,
+        'X-Goog-FieldMask': 'places.displayName'
+      },
+      body: JSON.stringify(requestBody)
+    });
+
+    const data = await response.json();
+    console.log(data);
+    reply.send(data);
+
+  } catch (error) {
+    console.error('Error:', error);
+    reply.status(500).send({ error: 'An error occurred while fetching data' });
+  }
+});
 
 // Start the server and handle the promise
 server.listen({ port: 3000 })
